2c4b9c0f806cc720b5f81a148d10acdeb5dca3de
rajeshdevopsraja <120709883+rajeshdevopsraja@users.noreply.github.com>
2023-02-18 17:34:17 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -7,14 +7,9 @@ terraform {
   }
 }
 
-# Configure the AWS Provider
+# Configure the AWS Provider and authentication
 provider "aws" {
   region = "ap-southeast-1"
-}
-
-# aws authotication
-provider "aws" {
-  region     = "ap-southeast-1"
   access_key = "AKIARXGB6FAMIRV5FSWCy"
   secret_key = "MxqqoQ7eax8mT0euIlXU0NBJwizPpczuZcKtgIwk"
 }
